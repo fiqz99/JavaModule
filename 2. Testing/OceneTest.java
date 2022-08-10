8e319d2146daec0edcfa51f827ac1527203e797b
fiqz99 <66556674+fiqz99@users.noreply.github.com>
2022-08-10 11:42:45 +0200

    Update OceneTest.java

--- 2. Testing/OceneTest.java
+++ 2. Testing/OceneTest.java
@@ -1,9 +1,22 @@
+package unitTest;
 import static org.junit.Assert.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class OceneTest {
+	@BeforeClass
+	public static void beforeLog() {
+		Logger logger = Logger.getLogger(OceneTest.class.getName());
+		logger.log(Level.INFO, "Log before class.");
+	}
 	@Test
 	public void sixtyFiveReturnsD() {
+		System.out.println("First test");
 		var ocena = new Ocene();
 		assertEquals(ocena.determineGrade(65), 'D');
 	}
@@ -23,15 +36,32 @@ public class OceneTest {
 		assertEquals(ocena.determineGrade(83), 'B');
 	}
 	@Test
-	public void zeroReturnsF(){
+	public void seventyFiveReturnsC(){
 		var ocena = new Ocene();
-		assertEquals(ocena.determineGrade(0), 'F');
+		assertEquals(ocena.determineGrade(75), 'C');
 	}
+	@Test
+	public void ninetyTwoReturnsA(){
+		var ocena = new Ocene();
+		assertEquals(ocena.determineGrade(92), 'A');
+	}
+	@Test
+	public void overHundredReturnsSlash(){
+		var ocena = new Ocene();
+		assertEquals(ocena.determineGrade(120), '/');
+	}
+	@Test
 	public void negativeReturnsException(){
+		System.out.println("Last test");
 		var ocena = new Ocene();
 		assertThrows(IllegalArgumentException.class, 
 				() -> {
 					ocena.determineGrade(-1);
 				});
+	}
+	@AfterClass
+	public static void afterLog() {
+		Logger logger = Logger.getLogger(OceneTest.class.getName());
+		logger.log(Level.INFO, "Log after class.");
 	}
 }
