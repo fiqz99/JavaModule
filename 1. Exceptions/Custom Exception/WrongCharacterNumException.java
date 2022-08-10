ceacee29bf9fddb326cfd0048ef0b525666f87fe
fiqz99 <66556674+fiqz99@users.noreply.github.com>
2022-08-10 10:29:32 +0200

    Create WrongCharacterNumException.java

---
+++ 1. Exceptions/Custom Exception/WrongCharacterNumException.java
@@ -0,0 +1,10 @@
+public class WrongCharacterNumException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public WrongCharacterNumException(String errorMessage) {
+		super(errorMessage);
+	}
+}
